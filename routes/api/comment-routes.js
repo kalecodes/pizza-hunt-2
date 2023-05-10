198afdfab6ab0fc3166ab3ba6c6f4b7afd43d576
kalen510 <kalen.wiley@gmail.com>
2023-05-10 17:20:51 -0400

    connect reply logic to front end fixes #4

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
+// callback function of a 'route' method has 'req' and 'res' as parameters, so we dont have to expicitly pass any arguments to our methods
 // add a comment to a pizza
 router.route('/:pizzaId').post(addComment);
-// delete a comment from a pizza
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// delete a comment from a pizza or add a reply to a comment
+router.route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+// delete a reply from a comment
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 
 module.exports = router;
